6f19c6ae4784d6d077e009797e9858c96427de85
Evgeny Golichenkov <neonmaster22@yandex.ru>
2023-10-26 11:37:09 +0300

    [#007] fix Program.cs

--- Program.cs
+++ Program.cs
@@ -2,26 +2,31 @@
 using static System.Console;
 
 class ControlWork{
-    static void Main(){
+    public static void Main(){
         string[] myArray1 = new string[] {"Hello", "2", "world", ":-)"};
-        //string[] myArray2 = ["1234", "1567", "-2", "computer science"];
-        //string[] myArray3 = ["Russia", "Denmark", "Kazan"];
+        string[] myArray2 = new string[] {"1234", "1567", "-2", "computer science"};
+        string[] myArray3 = new string[] {"Russia", "Denmark", "Kazan"};
         //string[] myArray = new static[] {};
-        int lenString = 3;
+        int size = 3;
 
+        SearchArray(myArray1, size);
+        SearchArray(myArray2, size);
+        SearchArray(myArray3, size);
 
-        string[] result = new string [myArray1.Length];
-        for (int i = 0; i < myArray1.Length; i++){
-            if (myArray1[i].Length <= lenString) {
-                result[i] = myArray1[i];
+        void SearchArray(string[] myArray, int lenString){
+            string[] result = new string [myArray.Length];
+            for (int i = 0; i < myArray.Length; i++){
+                if (myArray[i].Length <= lenString) {
+                    result[i] = myArray[i];
+                }
             }
-        }
 
-        WriteLine($"Массив из строк не длинее трех символов");
-        foreach (string ch in result){
-            Write($"{ch} ");
-        }
-        WriteLine("");
+            WriteLine($"Массив из строк не длинее трех символов");
+            foreach (string ch in result){
+                Write($"{ch} ");
+            }
+            WriteLine("");
+            }
     }
 }
 
